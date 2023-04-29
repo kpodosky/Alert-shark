013e4496e43a339b4da33e3dd408913dd4bd2b3d
Lanko <kpodosky@users.noreply.github.com>
2023-04-29 12:47:27 +0100

    Delete clock.py

--- clock.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-sched = BlockingScheduler()
-
-#execute very 30 minutes
-@sched.scheduled_job('interval', minutes=30)
-def scheduled_job():
-#start logic
- sched.start()
